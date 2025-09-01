ba0991e59208558383c03ebb295f96d9d215499b
guangyu.chen <cgy32321@163.com>
2025-09-01 15:49:40 +0800

    first commit

---
+++ src/pages/test.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "@theme/Layout";
+
+
+const test = () => {
+    return (
+        <Layout>
+            <h1>Hello, Woccccccccccccrld!</h1>
+        </Layout>
+    );
+};
+
+export default test;
